496554f4f75fa84e27d05f9252689a4a82df39b8
Tully Foote <tfoote@osrfoundation.org>
2014-08-05 15:04:42 -0700

    adding timer and resource measurements

--- watchprocess.py
+++ watchprocess.py
@@ -7,6 +7,15 @@ import subprocess
 import sys
 import tempfile
 
+# Known deprecated but no alternative with python 2 compatability. 
+from contextlib import nested
+
+
+# Timer
+import time
+
+# Resources
+import resource
 
 class MonitorError(Exception):
     """ Base class for exceptions"""
@@ -20,6 +29,51 @@ class PathError(MonitorError):
 def basename_equal(path1, path2):
     return os.path.basename(path1) == os.path.basename(path2)
 
+config = {}
+results = {}
+context_managers = []
+
+class Timer:
+
+    def __init__(self, results, max=None):
+        self.timeout = max
+        self.results = results
+    def __enter__(self):
+        self.start_time = time.time()
+    def __exit__(self, type, error, traceback):
+        finish_time = time.time()
+        elapsed_time = (finish_time - self.start_time)
+        self.results['start_time'] = self.start_time
+        self.results['finish_time'] = finish_time
+        self.results['elapsed_time'] = elapsed_time
+
+context_managers.append(Timer(results))
+
+
+class Resources:
+    def __init__(self, results):
+        self.results = results
+        
+    def __enter__(self):
+        pass
+    def __exit__(self, type, error, traceback):
+        usage = resource.getrusage(resource.RUSAGE_CHILDREN)
+        self.results['user_cpu'] = usage.ru_utime
+        self.results['system_cpu'] = usage.ru_stime
+        self.results['resident_memory_size'] = usage.ru_maxrss
+        # not in linux self.results['shared_memory_size'] = usage.ru_ixrss
+        # not in linux self.results['unshared_memory_size'] = usage.ru_idrss
+        # not in linux self.results['unshared_stack_size'] = usage.ru_isrss
+        self.results['minor_page_fault'] = usage.ru_minflt
+        self.results['major_page_fault'] = usage.ru_majflt
+        self.results['swap_outs'] = usage.ru_nswap
+        self.results['block_inputs'] = usage.ru_inblock
+        self.results['block_outputs'] = usage.ru_oublock
+        self.results['voluntary_context_switches'] = usage.ru_nvcsw
+        self.results['involuntary_context_switches'] = usage.ru_nivcsw
+
+
+context_managers.append(Resources(results))
 
 def detect_next_path_instance(argv0, path):
     """ This will take an executable name and path and find the next
@@ -64,7 +118,14 @@ def rewrite_args_for_monitoring(args, path=None, env=None):
 new_args, new_env = rewrite_args_for_monitoring(sys.argv)
 print(">>>>Watchprocess Running %s on path %s" % (sys.argv, os.getenv('PATH')))
 print(">>>>Watchprocess Substituting %s on path %s" % (new_args, new_env['PATH']))
-retcode = subprocess.call(new_args, env=new_env)
 
+with nested(*context_managers):
+
+    retcode = subprocess.call(new_args, env=new_env)
+    
+
+print ">>>>Watchprocess Results:"
+for k, v in results.items():
+    print(">>>> %s: %s" % (k, v))
 
 sys.exit(retcode)
